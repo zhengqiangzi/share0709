23244d59c90243c44b765357c52307523d46433f
郑强子 <415095389@qq.com>
2022-05-16 09:03:33 +0800

    fixed

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var { graphqlHTTP } = require('express-graphql')
+var { buildSchema } = require('graphql')
+
+var schema = buildSchema(`
+  type Query {
+    hello: String,
+    k:Int
+  }
+`)
+
+var root = { hello: () => 'Hello world!', k: () => 1 }
+
+var app = express()
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'))
