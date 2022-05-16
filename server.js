b733310602b664c2d0a8ca5f083a81fb2ee76fbd
郑强子 <415095389@qq.com>
2022-05-16 14:22:31 +0800

    fixed

--- server.js
+++ server.js
@@ -1,22 +1,76 @@
 var express = require('express')
 var { graphqlHTTP } = require('express-graphql')
-var { buildSchema } = require('graphql')
+var graphql = require('graphql')
 
-var schema = buildSchema(`
-  type Query {
-    hello: String,
-    k:Int
-  }
-`)
+var fakeDataBase = {
+  a: {
+    id: 'a',
+    name: 'alice',
+  },
+  b: {
+    id: 'b',
+    name: 'bob',
+  },
+}
+let userType = new graphql.GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {
+      type: graphql.GraphQLString,
+    },
+    name: {
+      type: graphql.GraphQLString,
+    },
+  },
+})
+let queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      args: {
+        id: { type: graphql.GraphQLString },
+      },
+      resolve: (_, { id }) => {
+        return fakeDataBase[id] || null
+      },
+    },
+    gov: {
+      type: graphql.GraphQLBoolean,
+      deprecationReason: '这个是一个测试的',
+      resolve: () => {
+        return true
+      },
+    },
+  },
+})
 
-var root = { hello: () => 'Hello world!', k: () => 1 }
+let mutation = new graphql.GraphQLObjectType({
+  name: 'mutation',
+  fields: {
+    t: {
+      type: graphql.GraphQLInt,
+      args: {
+        id: {
+          type: graphql.GraphQLInt,
+        },
+      },
+      resolve: (_, { id }) => {
+        return 1
+      },
+    },
+  },
+})
 
+let schema = new graphql.GraphQLSchema({
+  query: queryType,
+  mutation: mutation,
+})
 var app = express()
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 )
